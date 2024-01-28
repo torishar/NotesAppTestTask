d6de7e5b4c39cfeedb84f04f771a0f0d785aa62d
torishar <v.sharukhina@yandex.ru>
2024-01-28 18:54:34 +0300

    Work with folders is configured

--- NotesAppTestTask/AppDelegates/AppDelegate.swift
+++ NotesAppTestTask/AppDelegates/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.path ?? "")
         return true
     }
 
--- NotesAppTestTask/Folders/FoldersTableViewController.swift
+++ NotesAppTestTask/Folders/FoldersTableViewController.swift
@@ -7,13 +7,75 @@
 
 import Foundation
 import UIKit
+import RealmSwift
 
 class FoldersTableViewController: UITableViewController {
+    
+    var alert = UIAlertController()
+    let realm = try! Realm()
+    var folders: [FolderModel]?
+    let service = Service()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getAllFolders()
     }
-
-
+    
+    private func getAllFolders(){
+        self.folders = Array(realm.objects(FolderModel.self))
+        tableView.reloadData()
+    }
+    
+    
+    @IBAction func addFolder(_ sender: Any) {
+        alertAction()
+        present(alert, animated: true)
+    }
+    
+    private func alertAction() {
+        alert = UIAlertController(title: "Add folder", message: "", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Folder name"
+        }
+        
+        let alertAction = UIAlertAction(title: "Add", style: .default) { action in
+            if let text = self.alert.textFields?[0].text {
+                self.service.creatFolder(text)
+                self.getAllFolders()
+            }
+        }
+        
+        alert.addAction(alertAction)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return folders?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "folder", for: indexPath)
+        cell.textLabel?.text = folders?[indexPath.row].nameFolder
+        cell.detailTextLabel?.text = "\(folders?[indexPath.row].notes.count)"
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showNotes", let dest = segue.destination as? NotesTableViewController, let selectFolderIndex = tableView.indexPathForSelectedRow {
+            //
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            if let folder = folders?[indexPath.row]{
+                service.renoveFolder(folder)
+                folders?.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showNotes", sender: nil)
+    }
 }
