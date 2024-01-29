ac59692a129111cc6044e8155e9f741474288882
torishar <v.sharukhina@yandex.ru>
2024-01-30 01:56:17 +0300

    Added the ability to add and save images

--- NotesAppTestTask/Notes/Note/NoteViewController.swift
+++ NotesAppTestTask/Notes/Note/NoteViewController.swift
@@ -9,17 +9,19 @@ import UIKit
 import RealmSwift
 
 class NoteViewController: UIViewController {
-
+    
     @IBOutlet weak var noteTitle: UITextField!
     @IBOutlet weak var noteDescription: UITextView!
     var note: NoteModel?
     let service = Service()
     var folderId: ObjectId?
+    var selectImage: UIImage?
     
     @IBAction func saveNote(_ sender: Any) {
         let newNote = NoteModel()
         newNote.noteTitle = noteTitle.text ?? ""
         newNote.noteDescription = noteDescription.text ?? ""
+        newNote.imageUrl = saveImageToDocumentDirectory(selectImage)
         if note == nil {
             if folderId != nil {
                 service.createNote(folderId!, newNote)
@@ -27,8 +29,16 @@ class NoteViewController: UIViewController {
         } else {
             service.updateNote(note!, newNote: newNote)
         }
-        
         navigationController?.popViewController(animated: true)
+    }
+    
+    @IBOutlet weak var imageNote: UIImageView!
+    
+    @IBAction func addImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
@@ -36,9 +46,55 @@ class NoteViewController: UIViewController {
         if note != nil {
             noteTitle.text = note?.noteTitle
             noteDescription.text = note?.noteDescription
+            if let imageUrl = note?.imageUrl,
+               let image = loadImageFromDocumentDirectory(imageUrl) {
+                imageNote.image = image
+            }
+        }
+    }
+    
+    func saveImageToDocumentDirectory(_ image: UIImage?) -> String? {
+        guard let image = image,
+              let jpgImageData = image.jpegData(compressionQuality: 1.0) else {
+            return nil
+        }
+        
+        let fileName = UUID().uuidString + ".jpg"
+        let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent(fileName)
+        
+        do {
+            try jpgImageData.write(to: fileURL)
+            return fileName
+        } catch {
+            return nil
+        }
+    }
+    
+    func loadImageFromDocumentDirectory(_ imageUrl: String) -> UIImage? {
+        let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent(imageUrl)
+        do {
+            let imageData = try Data(contentsOf: fileURL)
+            return UIImage(data: imageData)
+        } catch {
+            return nil
         }
     }
 
-
 }
+
+extension NoteViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            selectImage = pickedImage
+            imageNote.image = pickedImage
+        }
+        dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
 
